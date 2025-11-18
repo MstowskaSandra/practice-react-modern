f0c9190e71ae0e10e87ddc982a8620e2e5efec5a
Sandra <sandra.mstowskaa@gmail.com>
2025-11-18 16:55:40 +0100

    task04: feat: add functions checCompatability, hendleKeyDown, startAgain in SpeedTest.js and add timerHook

--- 04/SpeedTest.js
+++ 04/SpeedTest.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import useRandomItem from './hook';
+import useTimer from './timerHook';
 
 function SpeedTest() {
     const [word, regenerateWord] = useRandomItem(['devmentor.pl', 'abc', 'JavaScript']);
     const [inputValue, setInputValue] = useState('');
     const [characters, setCharacters] = useState(0);
+    const [isInputDisabled, setInputDisabled] = useState(false);
+    const Timer = useTimer(30);
 
     useEffect(() => {
         regenerateWord();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useEffect(() => {
+        if (Timer.value === 0) {
+            setInputDisabled(true);
+        }
+    }, [Timer.value]);
 
     const checkCompatability = () => {
         if (word === inputValue) {
@@ -25,15 +34,33 @@ function SpeedTest() {
         }
     };
 
+    const startAgain = () => {
+        regenerateWord();
+        setCharacters(0);
+        setInputValue('');
+        setInputDisabled(false);
+        Timer.reset();
+    };
+
     return (
         <div>
+            <p>{Timer.value}</p>
             <h1>{word}</h1>
             <input
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 onKeyDown={handleKeyDown}
+                disabled={isInputDisabled}
+                onFocus={() => {
+                    if (!isInputDisabled) {
+                        Timer.start();
+                    }
+                }}
             />
             <p>Points: {characters}</p>
+            <button type="button" onClick={() => startAgain()}>
+                Again
+            </button>
         </div>
     );
 }
--- 04/timerHook.js
+++ 04/timerHook.js
@@ -1,21 +1,37 @@
-import { useEffect, useState } from "react";
+import { useCallback, useRef, useState } from 'react';
 
-const useTimer = seconds => {
-    const [counter, setCounter] = useState(seconds);
-    useEffect(() => {
-        const id = setInterval(() => {
-            setCounter(currCounter => {
-                const nextCounter = currCounter - 1;
-                if(nextCounter === 0) {
-                    clearInterval(id);
+const useTimer = (initailSeconds) => {
+    const [counter, setCounter] = useState(initailSeconds);
+    const intervalId = useRef(null);
+    const initial = useRef(initailSeconds);
+
+    const clear = () => {
+        if (intervalId.current) {
+            clearInterval(intervalId.current);
+            intervalId.current = null;
+        }
+    };
+
+    const start = useCallback(() => {
+        clear();
+        setCounter(initial.current);
+        intervalId.current = setInterval(() => {
+            setCounter((curr) => {
+                if (curr <= 1) {
+                    clear();
+                    return 0;
                 }
-                return nextCounter;
+                return curr - 1;
             });
         }, 1000);
-
-        return () => clearInterval(id)
     }, []);
-    return counter;
-}
+
+    const reset = useCallback(() => {
+        clear();
+        setCounter(initial.current);
+    }, []);
+
+    return { value: counter, reset, start };
+};
 
 export default useTimer;
